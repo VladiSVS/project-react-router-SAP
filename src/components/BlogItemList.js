92ab9027a4a02abfae287437e543eda02e96123c
VladiSVS <uladzimir.siauko@gmail.com>
2021-09-21 09:29:18 +0200

    sap

--- src/components/BlogItemList.js
+++ src/components/BlogItemList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import blogData from '../data/data';
+import { Link } from 'react-router-dom';
 
 const flexy = {
     display: 'flex',
@@ -17,7 +18,8 @@ class BlogItemList extends React.Component {
         console.log(this.props.match.params.id)
         return <section style={flexy}>
             <div style={{ maxWidth: "40vw" }}>
-                <img src={this.state.blogData[0].img_url} style={{ borderRadius: "1em", width: "100%", height: "50vh", objectFit: "cover" }} alt=""></img>
+                <Link style={{ textDecoration: 'none', textAlign: 'left', fontWeight: "bold", fontSize: "1.1em" }} to="/blog">&#171;back</Link>
+                <img src={this.state.blogData[0].img_url} style={{ borderRadius: "1em", width: "100%", height: "50vh", marginTop: "1em", objectFit: "cover" }} alt=""></img>
                 <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                     <h1 style={{ fontSize: "2.5em" }}>{this.state.blogData[0].title}</h1>
                     <h1 style={{ fontSize: "1.2em" }}>{this.state.blogData[0].published_date}</h1>
